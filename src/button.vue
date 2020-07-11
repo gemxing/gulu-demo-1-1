ffdcb00016a4e0013ae9fbe3a41fb8a51fad1d21
potS <1820346204@qq.com>
2020-07-11 18:57:13 +0800

    使用 npm test 和 npm dev-test 自动测试

--- src/button.vue
+++ src/button.vue
@@ -9,8 +9,12 @@
 </template>
 
 <script>
+import Icon from './icon'
     export default {
         // props: ['icon','iconPosition']//接收外部传参
+        components:{
+            'g-icon': Icon
+        },
         props:{
             icon:{},
             loading:{
