990f47743dbf182b7111dc76388b1c76fdafa789
potS <1820346204@qq.com>
2020-07-26 21:59:34 +0800

    add row and col

--- src/app.js
+++ src/app.js
@@ -4,10 +4,12 @@ import Icon from './icon'
 import ButtonGroup from './button-group.vue'
 import Input from './input'
 
+
 Vue.component('g-button',Button)
 Vue.component('g-icon',Icon)
 Vue.component('g-button-group',ButtonGroup)
 Vue.component('g-input',Input)
+
 
 new Vue({
     el: '#app',
